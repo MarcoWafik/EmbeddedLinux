231369cc7ad797582cebb8a266afe3ac916b6356
Marco Wafik <marcowafik15@gmail.com>
2024-03-13 04:21:00 +0200

    Second_Task

---
+++ AdminLinux_Task2/BinarySearch.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+int BinarySearch ( int* ptrToArray , int arraySize , int searchedElement )
+{
+	int leftPtr =  0;
+	int rightPtr = arraySize - 1;
+	int midPtr = (rightPtr + leftPtr) / 2 ;
+
+	while (searchedElement != ptrToArray[midPtr])
+	{
+
+	 if (searchedElement > ptrToArray[leftPtr])
+		{
+			leftPtr = midPtr;
+			midPtr = (rightPtr +  leftPtr) / 2;
+		}
+		else if (searchedElement < ptrToArray[rightPtr])
+		{
+			rightPtr = midPtr;
+			midPtr = (rightPtr +  leftPtr) / 2;
+		}
+	}
+
+	return midPtr;
+
+}
+
+
+
+int main()
+{
+	int arr[] = {1,2,3,4};
+	printf("%d\n",BinarySearch (arr , 4 ,4));
+}
+
+
+
+
+	
+
+
+
+
+
